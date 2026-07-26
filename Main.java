86475715a8cad3b047705584664e51d9f8fea264
Jun Yeon <ziririsky8@naver.com>
2026-07-26 19:33:03 +0900

    Java Day2 - if문 완

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
-        int age = 25;
-        int score = 80;
+int score = 50;
 
-        System.out.println(age >= 20);
-        System.out.println(score >= 90);
-        System.out.println(age >= 20 && score >= 90);
-        System.out.println(age >= 20 || score >= 90);
-        System.out.println(!(score >= 90));
+if (score >= 60) {
+    System.out.println("합격");
+}
+
+System.out.println("공부했다.");
+
+System.out.println("시험 종료");
     }
 }   
