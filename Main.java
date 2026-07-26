fd42e20e351baa11abd693df7f4e43d8c863ecfd
Jun Yeon <ziririsky8@naver.com>
2026-07-26 19:40:53 +0900

    Java Day2 - switch 문 완

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-int score = 50;
+int month = 7;
 
-if (score >= 60) {
-    System.out.println("합격");
+switch (month) {
+
+    case 12:
+    case 1:
+    case 2:
+        System.out.println("겨울");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        System.out.println("봄");
+        break;
+
+    case 6:
+    case 7:
+    case 8:
+        System.out.println("여름");
+        break;
+
+    default:
+        System.out.println("가을");
 }
-
-System.out.println("공부했다.");
-
-System.out.println("시험 종료");
     }
 }   
