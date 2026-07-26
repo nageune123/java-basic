135d2471be4b246a5c78437269bd6cb94e0edffc
Jun Yeon <ziririsky8@naver.com>
2026-07-26 19:19:57 +0900

    Java Day1 - 비교연산자 논리연산자 완

--- Main.java
+++ Main.java
@@ -1,20 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
+        
+        int age = 25;
+        int score = 80;
 
-        System.out.println("더하기 : " + (a + b));
-        System.out.println("빼기 : " + (a - b));
-        System.out.println("곱하기 : " + (a * b));
-        System.out.println("나누기 : " + (a / b));
-        System.out.println("나머지 : " + (a % b));
-
-        a++;
-
-        System.out.println("증가 : " + a);
-
-        a += 5;
-
-        System.out.println("복합 연산 : " + a);
+        System.out.println(age >= 20);
+        System.out.println(score >= 90);
+        System.out.println(age >= 20 && score >= 90);
+        System.out.println(age >= 20 || score >= 90);
+        System.out.println(!(score >= 90));
     }
 }   
