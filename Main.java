0f7fcae7a3e89b181c0720545678777f09319d19
Jun Yeon <ziririsky8@naver.com>
2026-07-26 19:02:12 +0900

    Java Day1 - Varialbes

--- Main.java
+++ Main.java
@@ -5,9 +5,14 @@ public class Main {
         double height = 170.5;
         boolean student = true;
 
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(height);
-        System.out.println(student);
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age);
+        System.out.println("키 : " + height);
+        System.out.println("학생 여부 : " + student);
+
+        age = 38;
+
+        System.out.println("1년 후 나이 : " + age);
+
     }
 }   
