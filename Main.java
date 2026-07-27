31e5a955aeb69bda96e74d08551157e1e27dd762
Jun Yeon <ziririsky8@naver.com>
2026-07-27 16:54:14 +0900

    Java Day3 - 배열 완

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-for (int i = 1; i <= 5; i++) {
+int[] arr = {2, 4, 6};
 
-    for (int j = 1; j <= 5 - i; j++) {
-        System.out.print(" ");
-    }
-
-    for (int j = 1; j <= i; j++) {
-        System.out.print("*");
-    }
-
-    System.out.println();
+for (int num : arr) {
+    System.out.println(num);
 }
     }
 }   
