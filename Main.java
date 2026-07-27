0795226f61c71c8cfab3a389c33cc94ff06590ca
Jun Yeon <ziririsky8@naver.com>
2026-07-27 16:39:57 +0900

    Java Day3 - for문 별찍기

--- Main.java
+++ Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
 for (int i = 1; i <= 5; i++) {
 
-    if (i % 2 == 0) {
-        continue;
+    for (int j = 1; j <= 5 - i; j++) {
+        System.out.print(" ");
     }
 
-    System.out.println(i);
+    for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+    }
+
+    System.out.println();
 }
     }
 }   
