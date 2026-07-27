47da42dedddc00d3a6f53e0222b0b371e2b37aef
Jun Yeon <ziririsky8@naver.com>
2026-07-27 18:17:48 +0900

    Java Day3 - collection

--- Main.java
+++ Main.java
@@ -1,31 +1,33 @@
-class Animal {
-
-    void sound() {
-        System.out.println("동물");
-    }
-
-}
-
-class Dog extends Animal {
-
-    @Override
-    void sound() {
-        System.out.println("멍멍");
-    }
-
-    void bark() {
-        System.out.println("왈왈");
-    }
-
-}
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Animal a = new Dog();
+       List<String> list = new ArrayList<>();
 
-        a.sound();
+list.add("사과");
+list.add("바나나");
 
+System.out.println(list.get(1));
+Set<String> set = new HashSet<>();
+
+set.add("A");
+set.add("B");
+set.add("A");
+
+System.out.println(set.size());
+
+Map<String, Integer> map = new HashMap<>();
+
+map.put("사과", 1000);
+map.put("바나나", 2000);
+
+System.out.println(map.get("사과"));
     }
 }
