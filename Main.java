bb2d6a7ebc93ce3051e2aa1069850d64f2ef6d09
Jun Yeon <ziririsky8@naver.com>
2026-07-27 16:33:07 +0900

    Java Day3 - for문 연습

--- Main.java
+++ Main.java
@@ -1,29 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-int month = 7;
+for (int i = 1; i <= 5; i++) {
 
-switch (month) {
+    if (i % 2 == 0) {
+        continue;
+    }
 
-    case 12:
-    case 1:
-    case 2:
-        System.out.println("겨울");
-        break;
-
-    case 3:
-    case 4:
-    case 5:
-        System.out.println("봄");
-        break;
-
-    case 6:
-    case 7:
-    case 8:
-        System.out.println("여름");
-        break;
-
-    default:
-        System.out.println("가을");
+    System.out.println(i);
 }
     }
 }   
